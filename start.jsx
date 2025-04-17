507f2c4ee685c64ecf4288be31911f8eae918b3e
managanemeke <managanemeke@gmail.com>
2025-04-17 09:29:30 +0500

    feat: add gitignore and start script

---
+++ start.jsx
@@ -0,0 +1,5 @@
+var project = app.open(new File("C:\\Users\\manag\\Documents\\test3.aep"));
+
+var tempFile = new File("C:\\Users\\manag\\github\\after-effects-script\\saveSubstrates.jsx");
+
+$.evalFile(tempFile);
