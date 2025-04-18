323d89de7cd6fc517dcdc76aa3e93d29ba2d0a31
managanemeke <managanemeke@gmail.com>
2025-04-18 20:37:04 +0500

    fix: run render queue only once

--- saveSubstrates.jsx
+++ saveSubstrates.jsx
@@ -1,23 +1,31 @@
 (function() {
     var rootDirectory = app.project.file.parent;
     var substratesDirectory = rootDirectory.fsName + "/" + "substrates";
+    var highQualityWithAlpha = "High Quality with Alpha";
 
     saveSubstrates();
 
     function saveSubstrates() {
+        app.project.renderQueue.showWindow(false);
+        clearRenderQueue();
         var projectItems = app.project.items;
         for (var i = 1; i <= projectItems.length; i++) {
             var item = projectItems[i];
             if (item instanceof CompItem) {
-                var comp = item;
-                comp.openInViewer();
-                setTimeToFirstMarkerIfExists();
-                saveActiveFrameToPng();
+                addCompositionToRenderQueue(item);
             }
         }
+        runRenderQueue();
+        clearRenderQueue();
         if (projectItems.length > 0) {
             openSubstratesDirectory();
         }
+    }
+
+    function addCompositionToRenderQueue(comp) {
+        comp.openInViewer();
+        setTimeToFirstMarkerIfExists();
+        addActiveFrameToQueueAsPng();
     }
 
     function activeComp() {
@@ -50,10 +58,8 @@
         return new File(outputFolder.fsName + "/" + name);
     }
 
-    function saveActiveFrameToPng() {
+    function addActiveFrameToQueueAsPng() {
         var comp = activeComp();
-
-        //if the resolution isnt 'Full', store current resolution and set to Full, then restore later;
         var res = [1, 1];
         if (comp.resolutionFactor != "1,1") {
             res = comp.resolutionFactor;
@@ -63,50 +69,34 @@
         if (file.exists) {
             file.remove();
         }
-        //close the renderQueue panel
-        app.project.renderQueue.showWindow(false);
-        //backup the render queue status, then uncheck the queued items
-        var RQbackup = storeRenderQueue();
-        if (RQbackup[RQbackup.length - 1] == "rendering") {
-            alert(RQerr);
-        } else {
-            //call command "save frame as" to add current frame to render queue
-            app.executeCommand(2104);
-            app.project.renderQueue.item(app.project.renderQueue.numItems).render = true;
-            var templateTemp = app.project.renderQueue.item(app.project.renderQueue.numItems).outputModule(1).templates;
-            //call hidden template '_HIDDEN X-Factor 16 Premul', which exports png with alpha
-            var setPNG = app.project.renderQueue.item(app.project.renderQueue.numItems).outputModule(1).templates[templateTemp.length - 1];
-            app.project.renderQueue.item(app.project.renderQueue.numItems).outputModule(1).applyTemplate(setPNG);
-            app.project.renderQueue.item(app.project.renderQueue.numItems).outputModule(1).file = file;
-            var finalpath = app.project.renderQueue.item(app.project.renderQueue.numItems).outputModule(1).file.fsName;
-            app.project.renderQueue.render();
-            //remove the rendered item and restored the render queue items
-            app.project.renderQueue.item(app.project.renderQueue.numItems).remove();
-            if (RQbackup != null) {
-                restoreRenderQueue(RQbackup);
-            }
-            app.activeViewer.setActive();
-            comp.resolutionFactor = res;
-        }
+        callSaveFrameAs();
+        var queue = app.project.renderQueue;
+        var lastItem = queue.item(queue.numItems);
+        prepareQueueItemToSaveAsPngFile(lastItem, file);
+        app.activeViewer.setActive();
+        comp.resolutionFactor = res;
     }
 
-    function storeRenderQueue() {
-        var checkeds = [];
-        for (var p = 1; p <= app.project.renderQueue.numItems; p++) {
-            if (app.project.renderQueue.item(p).status == RQItemStatus.RENDERING) {
-                checkeds.push("rendering");
-                break;
-            } else if (app.project.renderQueue.item(p).status == RQItemStatus.QUEUED) {
-                checkeds.push(p);
-                app.project.renderQueue.item(p).render = false;
-            }
-        }
-        return checkeds;
+    function callSaveFrameAs() {
+        app.executeCommand(2104);
     }
 
-    function restoreRenderQueue(checkedItems) {
-        for (var q = 0; q < checkedItems.length; q++) {
-            app.project.renderQueue.item(checkedItems[q]).render = true;
+    function prepareQueueItemToSaveAsPngFile(item, file) {
+        item.render = true;
+        var outputModule = item.outputModule(1);
+        outputModule.applyTemplate(highQualityWithAlpha);
+        outputModule.file = file;
+    }
+
+    function runRenderQueue() {
+        var queue = app.project.renderQueue;
+        queue.render();
+    }
+
+    function clearRenderQueue() {
+        var queue = app.project.renderQueue;
+        while (queue.numItems > 0) {
+            queue.item(queue.numItems).remove();
         }
     }
 })();
